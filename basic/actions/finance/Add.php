e97930f9ff233923715576b4bd4ff3ac1e1d8783
zhanghuimin <zhanghuimin0125@outlook.com>
2018-07-01 21:58:50 +0800

    新增

--- basic/actions/finance/Add.php
+++ basic/actions/finance/Add.php
@@ -8,24 +8,28 @@ use app\models\FinanceDetail;
 class add extends yii\base\Action {
     
     public function run() {
-        $post = Yii::$app->request->post();
-        
-        $valid_post = $this->checkPost($post);
-        
-        $model = new FinanceDetail();
-        $model->finance_name = $valid_post['fiance_name'];
-        $model->finance_money = $valid_post['fiance_money'];
-        $model->finance_time = $valid_post['create_time'];
-        
-        if($model->save(false)){
-            return json_encode(array('code'=>'0','isSuccess'=>true));
+        try{
+            $post = Yii::$app->request->post();
+            $valid_post = $this->checkPost($post);
+            
+            $model = new FinanceDetail();
+            $model->finance_name = $valid_post['fiance_name'];
+            $model->finance_money = $valid_post['fiance_money'];
+            $model->finance_time = $valid_post['create_time'];
+            
+            if($model->save(false)){
+                return json_encode(array('code'=>'0'));
+            }          
+            
+        }catch(Exception $e){
+            return json_encode(array('code'=>'-1'));
         }
-           
+    
     }
     
     private function checkPost($post){
         //此处需要验证一些参数，如金额是否是数字等，暂省
-        return $post;
+        return json_decode($post);
     }
 
 
